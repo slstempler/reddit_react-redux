5bb63d8803a9822ab4ab179c01e8a7bb1bf0ce04
slstempler <29709880+slstempler@users.noreply.github.com>
2022-07-21 16:01:53 -0400

    updated posts page styling and improved image URL handling

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -1,5 +1,6 @@
+import { unwrapResult } from "@reduxjs/toolkit";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./posts.css"
 
 export const Post = ({postData}) => {
@@ -7,20 +8,59 @@ export const Post = ({postData}) => {
     const routeUrl = `/r/${postData.subreddit}/comments/${postData.id}`;
     let thumbUrl;
     if(postData.thumbnail_height) thumbUrl = postData.thumbnail;
+    let navigate = useNavigate()
+
+    // returns image for Card
+    const findfirstGalleryImageUrl = (obj) => {
+        const imageKeys = Object.keys(obj);
+        const firstImageKey = imageKeys[0];
+        let firstImageObj = obj[firstImageKey];
+        let unparsedUrl = firstImageObj.p[firstImageObj.p.length-1].u;
+        return unparsedUrl.replace(/&amp;/g, '&');
+    }
+
+    // event handler for Go To Thread button
+    const navToThread = (e) => {
+        e.preventDefault();
+        navigate(routeUrl);
+    }
+
     //console.log(postData.title);
     //console.log(postData);
     return (
-        // Post HAS thumbnail
-        // Post does NOT have thumbnail
-        // Post is NSFW
-        // Post is a Spoiler
-        <div className="posts-post">
-            {thumbUrl && thumbUrl !== 'spoiler' && thumbUrl !== 'nsfw' && <img src={thumbUrl}></img>}
-            {thumbUrl === 'spoiler' && <h5>Shh... Link is a Spoiler!</h5>}
-            {thumbUrl === 'nsfw' && <h5>Hmmm... Link is NSFW</h5>}
-            <br/>
-            <p className="posts-post-score">{postData.score} | </p><a href={postUrl} className="posts-post-link">{postData.title}</a>
-            <Link className="posts-post-thread" to={routeUrl}>Go to Thread</Link>
+        <div className="posts-post" onClick={navToThread}>
+        {/* Post is NOT a gallery */}
+            {thumbUrl && thumbUrl !== 'spoiler' && thumbUrl !== 'nsfw' && !postData.is_gallery &&
+                <>
+                    <span className="posts-post-score">{postData.score}</span>
+                    <img src={postData.url}></img>
+                    <a href={postUrl} className="posts-post-link">{postData.title}</a>
+                    <button type="button" onClick={navToThread} className="posts-post-thread">Go To Thread</button> 
+                </>
+            }
+        {/* Post IS a gallery */}
+            {postData.is_gallery && thumbUrl !== 'nsfw' && thumbUrl !== 'spoiler' &&
+                <>
+                    <span className="posts-post-score">{postData.score}</span>
+                    <img src={findfirstGalleryImageUrl(postData.media_metadata)}></img>
+                    <a href={postUrl} className="posts-post-link">{postData.title}</a>
+                    <button type="button" onClick={navToThread} className="posts-post-thread">Go To Thread</button> 
+                </>
+            }
+        {/* Post does NOT have thumbnail */}
+            {(!thumbUrl || thumbUrl === 'nsfw' || thumbUrl === 'spoiler') &&
+                <>
+                    <span className="posts-post-score">{postData.score}</span>
+                    {thumbUrl === 'nsfw' &&  
+                        <h5 className="posts-post-nsfw">Hmmm... Link is NSFW</h5>
+                    }
+                    {thumbUrl === 'spoiler' && 
+                        <h5 className="posts-post-spoiler">Shh... Link is a Spoiler!</h5>
+                    }
+                    <a href={postUrl} className="posts-post-nothumb">{postData.title}</a>
+                    <button type="button" onClick={navToThread} className="posts-post-thread">Go To Thread</button>                
+                </>
+            }
         </div>
     );
 }
